ee9956276f22ffff50cd664a9d3f127a8386cd03
alaraby-tv <pmarkowski@alaraby.tv>
2021-03-18 12:40:37 +0000

    add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :invitable, :timeoutable, timeout_in: 1.hour
+         :recoverable, :rememberable, :trackable, :confirmable, :timeoutable, timeout_in: 1.hour
+
+  has_many :viewing_forms
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
