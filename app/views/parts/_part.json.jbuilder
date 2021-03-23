26881f01ad34f0894284a1e9eb47ca7ec0e64e32
alaraby-tv <pmarkowski@alaraby.tv>
2021-03-23 09:44:14 +0000

    deleted parts views

--- app/views/parts/_part.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! part, :id, :in, :out, :duration, :notes, :viewing_form_id, :created_at, :updated_at
-json.url part_url(part, format: :json)
--- spec/factories/parts.rb
+++ spec/factories/parts.rb
@@ -1,9 +1,5 @@
 FactoryBot.define do
   factory :part do
-    in { "MyString" }
-    out { "MyString" }
-    duration { "MyString" }
-    notes { "MyText" }
-    viewing_form { nil }
+    
   end
 end
