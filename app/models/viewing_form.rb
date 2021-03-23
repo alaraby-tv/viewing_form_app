7e0228014076518e9520a85f5ea2cefaad93670f
alaraby-tv <pmarkowski@alaraby.tv>
2021-03-23 09:52:07 +0000

    allow nested attributes for parts

--- app/models/viewing_form.rb
+++ app/models/viewing_form.rb
@@ -1,5 +1,7 @@
 class ViewingForm < ApplicationRecord
   belongs_to :user
   has_many :parts
-  accepts_attributes_for :parts
+  accepts_nested_attributes_for :parts, allow_destroy: true, reject_if: proc { |att| att['duration'].blank? }
+
+  validates_presence_of :program_title, :ingest_date, :tx_date, :program_id, :parts
 end
