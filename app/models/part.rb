48ac27bf65478ca60f15a1f18ac24938575d95bc
alaraby-tv <pmarkowski@alaraby.tv>
2021-03-26 12:22:35 +0000

    validate parts with regexp

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,3 +1,5 @@
 class Part < ApplicationRecord
-  belongs_to :viewing_form
+  belongs_to :viewing_form, inverse_of: :parts
+
+  validates_format_of :start, :finish, :duration, with: /^\d{2}:\d{2}:\d{2}:([01]\d|2[0-4])$/, on: :create, multiline: true
 end
--- app/models/viewing_form.rb
+++ app/models/viewing_form.rb
@@ -1,7 +1,11 @@
 class ViewingForm < ApplicationRecord
   belongs_to :user
-  has_many :parts
-  accepts_nested_attributes_for :parts, allow_destroy: true, reject_if: proc { |att| att['duration'].blank? }
+  has_many :parts, inverse_of: :viewing_form
+  accepts_nested_attributes_for :parts, reject_if: :reject_parts
 
-  validates_presence_of :program_title, :ingest_date, :tx_date, :program_id, :parts
+  def reject_parts(attributes)
+    !attributes['duration'] == /^\d{2}:\d{2}:\d{2}:([01]\d|2[0-4])$/
+  end
+
+  validates_presence_of :program_title, :ingest_date, :tx_date, :program_id, :contact_number, :parts
 end
