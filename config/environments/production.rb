0660f2923ec3ce7874bac2faf5835920af544dd6
alaraby-tv <pmarkowski@alaraby.tv>
2021-04-01 11:38:56 +0100

    changed authentication from "plain" to :plain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
 
-  config.action_mailer.default_url_options = { host: 'alaraby.tv' }
+  config.action_mailer.default_url_options = { host: 'viewingform.alaraby.tv' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default charset: "utf-8"
   config.action_mailer.raise_delivery_errors = true
@@ -77,9 +77,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => ENV["ALARBY_GMAIL_USERNAME"],
-    :password             => ENV["ALARABY_GMAIL_PASSWORD"],
-    :authentication       => "plain",
+    :domain               => "alaraby.tv",
+    :user_name            => ENV["GMAIL_USERNAME"],
+    :password             => ENV["GMAIL_PASSWORD"],
+    :authentication       => :plain,
     :enable_starttls_auto => true
   }
 
