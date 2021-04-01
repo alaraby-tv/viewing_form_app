2370307452da29d6a4e3edea23d5ee1e97a256e6
alaraby-tv <pmarkowski@alaraby.tv>
2021-04-01 11:55:31 +0100

    authenticate admin

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -9,7 +9,9 @@ module Admin
     before_action :authenticate_admin
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+      unless current_user && current_user.admin?
+        redirect_to root_path, error: "Access Denied!"
+      end
     end
 
     # Override this value to specify the number of elements to display at a time
