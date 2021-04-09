144f78026e19457da8b1e536da1f5d481115e54f
alaraby-tv <pmarkowski@alaraby.tv>
2021-04-09 11:53:05 +0100

    make first and last name required

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :validatable,
          :recoverable, :rememberable, :trackable, :confirmable, :timeoutable, timeout_in: 1.hour
 
   has_many :viewing_forms
+  validates :first_name, :last_name, presence: :true
 
   def full_name
     "#{first_name} #{last_name}"
