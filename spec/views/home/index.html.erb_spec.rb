aa749e2c8d7f2234c0f104d29b1c3832bed6b842
alaraby-tv <pmarkowski@alaraby.tv>
2021-04-26 10:40:25 +0100

    adjust the greeting mesage

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "home/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "renders a welcome message" do
+    render
+    assert_select "h1", text: "Welcome!"
+  end
 end
