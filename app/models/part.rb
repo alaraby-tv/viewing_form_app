eb5a35f177c7cda13ee19bb0ee20240f0c0e1cd5
alaraby-tv <pmarkowski@alaraby.tv>
2021-04-26 09:35:21 +0100

    add validations

--- app/models/part.rb
+++ app/models/part.rb
@@ -2,4 +2,5 @@ class Part < ApplicationRecord
   belongs_to :viewing_form, inverse_of: :parts
 
   validates_format_of :start, :finish, :duration, with: /^\d{2}:\d{2}:\d{2}:([01]\d|2[0-4])$/, on: :create, multiline: true
+  validates_presence_of :start, :finish, :duration
 end
