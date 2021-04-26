aa0fc5aaaf6f5b8976032bcd7beff08374c65528
alaraby-tv <pmarkowski@alaraby.tv>
2021-04-26 10:31:16 +0100

    create an instance of viewing form with FactoryBot and add it as na argument when sending email

--- spec/mailers/viewing_form_spec.rb
+++ spec/mailers/viewing_form_spec.rb
@@ -2,16 +2,17 @@ require "rails_helper"
 
 RSpec.describe ViewingFormMailer, type: :mailer do
   describe "send_form" do
-    let(:mail) { ViewingFormMailer.send_form }
+    let(:form) { FactoryBot.create(:viewing_form) }
+    let(:mail) { ViewingFormMailer.send_form(form) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Send form")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.subject).to eq("Viewing Form #{form.program_title} #{form.program_id}")
+      expect(mail.to).to eq(['mcringest@alaraby.tv', 'scheduling@alaraby.tv', 'playout@alaraby.tv', 'librarians@alaraby.tv'])
+      expect(mail.from).to eq(["server@alaraby.tv"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to match("Hello")
     end
   end
 
