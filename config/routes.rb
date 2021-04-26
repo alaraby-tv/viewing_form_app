c8c85a71da2c226dbe513613684395fdf9fdd945
alaraby-tv <pmarkowski@alaraby.tv>
2021-04-26 09:54:36 +0100

    remove route get /index

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   namespace :admin do
-      resources :users
-      resources :parts
-      resources :viewing_forms
+    resources :users
+    resources :parts
+    resources :viewing_forms
 
-      root to: "users#index"
-    end
+    root to: "users#index"
+  end
   resources :parts
   resources :viewing_forms
   root 'home#index'
-  get '/index' => 'home#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
