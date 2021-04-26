c9dae3e4d95dda6d26797eb98bb4c8da891237f1
alaraby-tv <pmarkowski@alaraby.tv>
2021-04-26 10:45:21 +0100

    adjust the items to be checked

--- spec/views/viewing_forms/show.html.erb_spec.rb
+++ spec/views/viewing_forms/show.html.erb_spec.rb
@@ -2,21 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "viewing_forms/show", type: :view do
   before(:each) do
-    @viewing_form = assign(:viewing_form, ViewingForm.create!(
-      user: nil,
-      program_title: "Program Title",
-      aspect_ratio: "Aspect Ratio",
-      program_id: "Program",
-      number_of_parts: 2
-    ))
+    @viewing_form = FactoryBot.create(:viewing_form)
   end
 
-  it "renders attributes in <p>" do
+  it "renders attributes" do
     render
-    expect(rendered).to match(//)
-    expect(rendered).to match(/Program Title/)
-    expect(rendered).to match(/Aspect Ratio/)
-    expect(rendered).to match(/Program/)
-    expect(rendered).to match(/2/)
+    expect(rendered).to match(/Programme title/)
+    expect(rendered).to match(/Aspect ratio/)
+    expect(rendered).to match(/Programme id/)
+    expect(rendered).to match(/No of parts/)
   end
 end
