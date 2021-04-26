d501c3902f145e3a092f462ac7a7293db7d92c66
alaraby-tv <pmarkowski@alaraby.tv>
2021-04-26 10:29:20 +0100

    add argument tot the method

--- spec/mailers/previews/viewing_form_preview.rb
+++ spec/mailers/previews/viewing_form_preview.rb
@@ -2,8 +2,8 @@
 class ViewingFormPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/viewing_form/send_form
-  def send_form
-    ViewingFormMailer.send_form
+  def send_form(form)
+    ViewingFormMailer.send_form(form)
   end
 
 end
