3a0a010a910c6201c9d5f5cc35b8be8393592e36
alaraby-tv <pmarkowski@alaraby.tv>
2021-04-27 16:23:29 +0100

    create login and viewing form forms with capybara specs

---
+++ spec/support/new_viewing_form.rb
@@ -0,0 +1,28 @@
+class NewViewingForm
+  include Capybara::DSL
+
+  def visit_page
+    visit('/viewing_forms')
+    click_on('New Viewing Form')
+    self
+  end
+
+  def fill_in_with(params = {})
+    fill_in('Programme Title', with: params.fetch(:program_title) )
+    fill_in('Enter Programme ID', with: params.fetch(:program_id) )
+    fill_in('IngestDate', with: params.fetch(:ingest_date) )
+    fill_in('TXDate', with: params.fetch(:tx_date) )
+    fill_in('ContactNo', with: params.fetch(:contact_number) )
+    click_link('add_association')
+    fill_in('Row1IN', with: params.fetch(:parts_attributes)[0][:start] )
+    fill_in('Row1OUT', with: params.fetch(:parts_attributes)[0][:finish] )
+    execute_script("$('#Row1DUR').val('#{params.fetch(:parts_attributes)[0][:duration]}')")
+    fill_in('Row1Notes', with: params.fetch(:parts_attributes)[0][:notes] )
+    self
+  end
+
+  def submit
+    click_on('Send Form')
+    self
+  end
+end
