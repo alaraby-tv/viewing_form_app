7eddfd99b2b21c8758a0ae2a4221d4dc0ff2a33e
alaraby-tv <pmarkowski@alaraby.tv>
2021-04-27 16:15:51 +0100

    change mailer delivery method to letter opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,16 +35,16 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default charset: "utf-8"
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => 'alaraby.tv',
-    :user_name            => ENV["GMAIL_USERNAME"],
-    :password             => ENV["GMAIL_PASSWORD"],
-    :authentication       => :plain,
-    :enable_starttls_auto => true
-  }
+  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.smtp_settings = {
+  #   :address              => "smtp.gmail.com",
+  #   :port                 => 587,
+  #   :domain               => 'alaraby.tv',
+  #   :user_name            => ENV["GMAIL_USERNAME"],
+  #   :password             => ENV["GMAIL_PASSWORD"],
+  #   :authentication       => :plain,
+  #   :enable_starttls_auto => true
+  # }
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
