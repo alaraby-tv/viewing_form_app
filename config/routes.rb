eb33f1ac6b62bcb26153e321add34437baf88819
alaraby-tv <pmarkowski@alaraby.tv>
2021-04-28 16:45:07 +0100

    create route to resend a viewing form

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
     root to: "users#index"
   end
   resources :parts
-  resources :viewing_forms
+  resources :viewing_forms do
+    member do
+      post :resend
+    end
+  end
   root 'home#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
