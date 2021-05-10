8a7cd01899f0574198fb5b73eef35cc8315bd98d
alaraby-tv <pmarkowski@alaraby.tv>
2021-05-10 11:28:06 +0100

    test if the form creator is ccd in the email

--- spec/mailers/viewing_form_spec.rb
+++ spec/mailers/viewing_form_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe ViewingFormMailer, type: :mailer do
+  include Rails.application.routes.url_helpers
+
   describe "send_form" do
     let(:form) { FactoryBot.create(:viewing_form) }
     let(:mail) { ViewingFormMailer.send_form(form) }
@@ -8,11 +10,12 @@ RSpec.describe ViewingFormMailer, type: :mailer do
     it "renders the headers" do
       expect(mail.subject).to eq("Viewing Form #{form.program_title} #{form.program_id}")
       expect(mail.to).to eq(['mcringest@alaraby.tv', 'scheduling@alaraby.tv', 'playout@alaraby.tv', 'librarians@alaraby.tv'])
+      expect(mail.cc).to eq([form.user.email])
       expect(mail.from).to eq(["server@alaraby.tv"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hello")
+      expect(mail.body.encoded).to include(viewing_form_url(form))
     end
   end
 
