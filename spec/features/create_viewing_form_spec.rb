74790bc364b666c43b88cdcbdd464f49988576e0
alaraby-tv <pmarkowski@alaraby.tv>
2021-05-10 11:26:19 +0100

    test adding and removing parts

--- spec/features/create_viewing_form_spec.rb
+++ spec/features/create_viewing_form_spec.rb
@@ -14,6 +14,8 @@ feature 'create new viewing form' do
       FactoryBot.attributes_for(:viewing_form, user: user)
     ).submit
 
+    expect(ActionMailer::Base.deliveries.last.cc).to match([user.email])
+    expect(ActionMailer::Base.deliveries.count).to eq(1)
     expect(page).to have_selector(:link_or_button, 'Print Form')
   end
 
@@ -22,6 +24,25 @@ feature 'create new viewing form' do
 
     new_viewing_form.visit_page.submit
     
+    expect(ActionMailer::Base.deliveries.count).to eq(0)
     expect(page).to_not have_selector(:link_or_button, 'Print Form')
+  end
+
+  scenario 'adds nested fields for viewing form parts', js: true do
+    login_form.visit_login_page.login_as(user)
+    new_viewing_form.visit_page.add_association
+
+    expect(page).to have_css("div.nested-fields", :count => 1)
+    expect(page).to have_css("a#remove_association")
+    expect(page).to_not have_css("a.li_1")
+  end
+
+  scenario 'removes nested fields from viewing form parts', js: true do
+    login_form.visit_login_page.login_as(user)
+    new_viewing_form.visit_page.add_association.remove_association
+
+    expect(page).to have_css("div.nested-fields", :count => 0)
+    expect(page).to have_css("a.li_1")
+    expect(page).to_not have_css("a#remove_association")
   end
 end
